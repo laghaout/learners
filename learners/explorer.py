1c5f5b88f41e1d41589c1a8e2c144ba288321160
laghaout <aknary@yahoo.com>
2021-10-20 14:06:10 +0200

    Remove extra stuff from the explorer.

--- learners/explorer.py
+++ learners/explorer.py
@@ -78,8 +78,9 @@ class Explorer:
             self.profile_report = ProfileReport(self.data)
             self.profile_report.to_file(os.path.join(
                 *[self.source_dir, f'{profile_report}.html']))
-            self.profile_report.to_file(os.path.join(
-                *[self.source_dir, f'{profile_report}.json']))
+            # Note that JSONs are up to 10 times heavier than the HTML reports.
+            # self.profile_report.to_file(os.path.join(
+            #     *[self.source_dir, f'{profile_report}.json']))
 
     def select_category(self, category, value):
 
@@ -122,110 +123,3 @@ class Explorer:
     def engineer(self):
 
         pass
-
-
-class Domain(Explorer):
-
-    def __init__(
-            self,
-            source=['domain', 'domain.scalar.tsv'],
-            usecols=[
-                'entity',
-                # Intrinsic beliefs
-                'campaignmonitor_result|Campaign Monitor',
-                '#Campaign Monitor.domain.belief|Campaign Monitor',
-                '#Quad9.domain.belief|Quad9',
-                '(Registered Domain|tldextract)#(Campaign Monitor.domain.belief|Campaign Monitor)',
-                '(Registered Domain|tldextract)#(Quad9.domain.belief|Quad9)',
-                '#DGABelief|Generic DGA Map',
-                '#DGABelief|dga_generator_bambenek-dga',
-                # Intrinsic features
-                '#BenignRank.domain.belief|tranco-list-top-million',
-                '#BenignRank.domain.belief|alexa-top-one-million',
-                '#BenignRank.domain.belief|cisco-umbrella-popularity-list',
-                '#BenignRank.domain.belief|majestic-million-top-websites',
-                'Top Level Domain|tldextract',
-                'domain_count|Farsight DNSDB',
-                'date (first seen)|Farsight DNSDB',
-                'date (last seen)|Farsight DNSDB',
-                'date (creation)|WHOIS',
-                'date (updated)|WHOIS',
-                '(Registered Domain|tldextract).(domain_count|Farsight DNSDB)',
-                '(Registered Domain|tldextract).(date (first seen)|Farsight DNSDB)',
-                '(Registered Domain|tldextract).(date (last seen)|Farsight DNSDB)',
-                '(Registered Domain|tldextract).(date (updated)|WHOIS)',
-                '(Registered Domain|tldextract)#(BenignRank.domain.belief|alexa-top-one-million)',
-                '(Registered Domain|tldextract)#(BenignRank.domain.belief|cisco-umbrella-popularity-list)',
-                '(Registered Domain|tldextract)#(BenignRank.domain.belief|majestic-million-top-websites)',
-                # Extrinsic beliefs
-                'DNSRecord (A)|Campaign Monitor',
-                '(DNSRecord (A)|NSLookup).(Abuse IP DB.IP address.confidence|Abuse IP DB)',
-                '(DNSRecord (A)|Campaign Monitor)#(Abuse IP DB.IP address.belief|Abuse IP DB)',
-                '(DNSRecord (A)|Farsight DNSDB)#(Abuse IP DB.IP address.belief|Abuse IP DB)',
-                '(DNSRecord (A)|NSLookup)#(Abuse IP DB.IP address.belief|Abuse IP DB)',
-                '(DNSRecord (A)|NSLookup)#(Campaign Monitor.domain.belief|Campaign Monitor)',
-                '(DNSRecord (A)|Farsight DNSDB)#(VirusTrackerInfection.ip.belief|Virustracker)',
-                '(DNSRecord (A)|NSLookup)#(VirusTrackerInfection.ip.belief|Virustracker)',
-                '(DNSRecord (A)|Farsight DNSDB).(Abuse IP DB.IP address.confidence|Abuse IP DB)',
-                # Extrinsic features
-                '(Registered Domain|tldextract).(DNSRecord (A)|Campaign Monitor)',
-                '(Registered Domain|tldextract).(DNSRecord (A)|Farsight DNSDB)',
-                '(Registered Domain|tldextract).(DNSRecord (MX)|Farsight DNSDB)',
-                '(Registered Domain|tldextract).(DNSRecord (NS)|Farsight DNSDB)',
-                '(Registered Domain|tldextract).(DNSRecord (SOA)|Farsight DNSDB)',
-                '(Registered Domain|tldextract).(DNSRecord (A)|NSLookup)',
-                '(Registered Domain|tldextract).(registrant_email|WHOIS)',
-                '(Registered Domain|tldextract).(registrant_name|WHOIS)',
-                'a_records_status|NSLookup',
-                '(DNSRecord (A)|NSLookup).(date (last seen)|Campaign Monitor)',
-                '(DNSRecord (A)|Campaign Monitor).(ASN|MaxMind GeoIP)',
-                '(DNSRecord (A)|Campaign Monitor).(Country Code (ISO)|MaxMind GeoIP)',
-                '(DNSRecord (A)|Farsight DNSDB).(Country Code (ISO)|Abuse IP DB)',
-                '(DNSRecord (A)|Farsight DNSDB).(ASN|MaxMind GeoIP)',
-                '(DNSRecord (A)|Farsight DNSDB).(Country Code (ISO)|MaxMind GeoIP)',
-                '(DNSRecord (A)|NSLookup).(Country Code (ISO)|Abuse IP DB)',
-                '(DNSRecord (A)|NSLookup).(ASN|MaxMind GeoIP)',
-                'DNSRecord (MX)|NSLookup',
-                'DNSRecord (MX)|Farsight DNSDB',
-                'DNSRecord (NS)|Farsight DNSDB',
-                'DNSRecord (SOA)|Farsight DNSDB',
-                'DNSRecord (TXT)|Farsight DNSDB',
-                'DNSRecord (NS)|NSLookup',
-                'DNSRecord (TXT)|NSLookup',
-                'DNSRecord (CNAME)|Farsight DNSDB',
-                'cname_records_status|NSLookup',
-                'mx_records_status|NSLookup',
-                'ns_records_status|NSLookup',
-                'DNS record(SOA) Lookup Status|NSLookup',
-                'txt_records_status|NSLookup'],
-            delimiter='\t',
-            index_col='entity',
-            **kwargs):
-
-        assert len(usecols) == len(set(usecols))
-
-        super().__init__(
-            source=source, usecols=usecols, delimiter=delimiter,
-            index_col=index_col, **kwargs)
-
-    def engineer(self):
-
-        # Features to be set to False if ``null`` and True otherwise.
-        nan_is_False = [
-            '#Campaign Monitor.domain.belief|Campaign Monitor',
-            '(Registered Domain|tldextract)#(Campaign Monitor.domain.belief|Campaign Monitor)',
-            '(Registered Domain|tldextract)#(Quad9.domain.belief|Quad9)',
-            '#DGABelief|Generic DGA Map',
-            '#DGABelief|dga_generator_bambenek-dga',
-        ]
-        self.data[nan_is_False] = self.data[nan_is_False].notna()
-
-        # Features to be converted to datetimes.
-        str_to_dt = [
-            'date (first seen)|Farsight DNSDB',
-            'date (last seen)|Farsight DNSDB',
-            'date (creation)|WHOIS',
-            'date (updated)|WHOIS',
-        ]
-        for feature in str_to_dt:
-            self.data[feature] = pd.to_datetime(self.data[feature])
