b43ca427c87a266a4b6bb8f09982174839f05254
laghaout <aknary@yahoo.com>
2023-05-23 14:30:54 +0200

    Updated the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='learners',
-    version='0.0.2',
+    version='0.0.3',
     author='Amine Laghaout',
     description='Object-oriented learners for machine learning.',
     packages=find_packages(exclude=('test*',)),
